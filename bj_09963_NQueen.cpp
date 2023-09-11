8babe387fe0893b21e380c9d141b14fa4ff0c52c
Hongju Lee <developerhongjulee@gmail.com>
2023-09-11 16:59:27 +0900

    230911: backtracking

--- bj_09963_NQueen.cpp
+++ bj_09963_NQueen.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, ret, visited_x[15], visited_dg_r[31], visited_dg_l[31];
-void bt(int k){ //한 줄에 하나씩 놓지 않으면 답안됨
+int n, ret, visited_x[15], visited_dg_r[29], visited_dg_l[29];
+void bt(int k){
     if(k==n) {ret++;return;}
     for(int v=0; v<n; v++){
-        if(visited_x[v]||visited_dg_r[k-v+n]||visited_dg_l[k+v]) continue;
-        visited_x[v]=visited_dg_r[k-v+n]=visited_dg_l[k+v]=true;
+        if(visited_x[v]||visited_dg_r[k-v+n-1]||visited_dg_l[k+v]) continue;
+        visited_x[v]=visited_dg_r[k-v+n-1]=visited_dg_l[k+v]=true;
         bt(k+1);
-        visited_x[v]=visited_dg_r[k-v+n]=visited_dg_l[k+v]=false;
+        visited_x[v]=visited_dg_r[k-v+n-1]=visited_dg_l[k+v]=false;
     }
 }
 int main(){
