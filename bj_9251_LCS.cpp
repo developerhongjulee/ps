b608f115dab1a5088bbc55bbe08030aba01a75f5
developerhongjulee <developerhongjulee@gmail.com>
2023-11-06 23:56:24 +0900

    231106 LCS

---
+++ bj_9251_LCS.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false), cin.tie(0)
+using namespace std;
+string s1,s2;
+vector<vector<int>> dp;
+int n,m;
+int main(){
+	fastio;
+	cin>>s1>>s2;
+	n=s1.size();
+	m=s2.size();
+	s1 = '0'+s1;
+	s2 = '0'+s2;
+	dp.resize(n+1,vector<int>(m+1,0));
+	for(int i(1); i<=n; i++){
+		for(int j(1); j<=m; j++){
+			dp[i][j]=s1[i]==s2[j]?
+				dp[i-1][j-1]+1:max(dp[i-1][j],dp[i][j-1]);
+		}
+	}
+	cout<<dp[n][m];
+}
